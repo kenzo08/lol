86fef5e7fe763b5e70d292454dd2e1565f634831
kenzo08 <50136009+kenzo08@users.noreply.github.com>
2021-09-28 16:13:00 +0500

    step

--- iq_practic/scripts/script.js
+++ iq_practic/scripts/script.js
@@ -11,15 +11,24 @@ $ ('.slider').slick({
 });
 let slider = document.querySelector('.slider');
 let list = document.querySelector('.list')
+let activ_but = document.querySelector('.activ_but_js')
 let yandex_map = document.querySelector('.yandex_map');
 let map = document.querySelector('.map');
    map.onclick = function(){
        slider.style.visibility = 'hidden';
+       list.style.backgroundColor='#FFFFFF';
+       list.style.color='black';
        yandex_map.style.visibility = 'visible';
+       map.classList.add('activ_but_js');
+       list.classList.remove('activ_but_js');
 
    }
 
    list.onclick = function (){
+   list.classList.add('activ_but_js');
+       list.style.backgroundColor=' #6BBD45';
+       list.style.color='#FFFFFF';
+       map.classList.remove('activ_but_js');
        slider.style.visibility = 'visible';
        yandex_map.style.visibility = 'hidden';
    }
