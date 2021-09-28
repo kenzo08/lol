ee508df58223635afbfb95af48a1126b265ded42
kenzo08 <50136009+kenzo08@users.noreply.github.com>
2021-09-28 10:20:21 +0500

    map

--- iq_practic/scripts/script.js
+++ iq_practic/scripts/script.js
@@ -1,7 +1,25 @@
-$('.slick-slider').slick({
-    infinite: true,
+$(document).ready(function(){
+$ ('.slider').slick({
+    dots: true,
+    infinite: false,
     slidesToShow: 3,
-    //количество слайдов, которые выводятся на экране
-    slidesToScroll: 2
-    //количество слайдов, которые перелистываются за один раз
+    adaptiveHeight: true,
+    speed: 400,
+    slidesToScroll: 1,
+
 });
+});
+let slider = document.querySelector('.slider');
+let list = document.querySelector('.list')
+let yandex_map = document.querySelector('.yandex_map');
+let map = document.querySelector('.map');
+   map.onclick = function(){
+       slider.style.visibility = 'hidden';
+       yandex_map.style.visibility = 'visible';
+
+   }
+
+   list.onclick = function (){
+       slider.style.visibility = 'visible';
+       yandex_map.style.visibility = 'hidden';
+   }
